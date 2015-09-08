1170426c0324083406c189652547e419348eed96
judychern <chernjudy@gmail.com>
2015-09-07 21:07:46 -0700

    Removed Math.min

--- hamming.js
+++ hamming.js
@@ -4,11 +4,11 @@ var counter = 0;
 if(input1.length !== input2.length){
 	throw new Error ('DNA strands must be of equal length.');
 }
-else{
-	var strandLength = Math.min(input1.length, input2.length);
-	for(i=0; i<strandLength; i++){
-		if(input1.charAt(i) !== input2.charAt(i)){
-			counter++;
+	else{
+		var strandLength = input1.length;
+			for(i=0; i<strandLength; i++){
+				if(input1.charAt(i) !== input2.charAt(i)){
+				counter++;
 		}
 	}
 }
